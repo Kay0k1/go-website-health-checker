8adf000e542bcfa96d323a4226f7a6ecf8d8bc6d
kay0k1 <yaroslav.belentiev@mail.ru>
2025-12-25 01:04:08 +0300

    add: headers

--- main.go
+++ main.go
@@ -19,6 +19,15 @@ type Result struct {
 }
 
 func CheckHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+	if r.Method == "OPTIONS" {
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+
 	if r.Method != http.MethodPost {
 		http.Error(w, "Only POST method is allowed", http.StatusMethodNotAllowed)
 		return
